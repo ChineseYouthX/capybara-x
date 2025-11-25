dc798b90da5f935f3fb5ef03f55aad2f945c3c7d
capybara-x <xiaxilin>
2025-11-25 18:25:52 +0800

    feat: web开发部署。

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,8 +1,32 @@
 import { sidebar } from 'vuepress-theme-hope'
 
 export const zhSidebar = sidebar({
-  '/': [],
-  '/other/rk/gx': 'structure',
-  '/front-end/': 'structure',
-  '/notes/': 'structure'
+  '/': [
+    {
+      text: 'web 开发',
+      link: 'web-dev/',
+      icon: 'code',
+      collapsible: true, // 是否可折叠
+      expanded: false, // 是否展开
+      children: [
+        {
+          text: 'nginx',
+          link: 'web-dev/nginx/'
+        },
+        {
+          text: '部署',
+          link: 'web-dev/deploy/'
+        },
+        {
+          text: 'Node JS',
+          link: 'web-dev/node-js/'
+        }
+      ]
+    },
+    {
+      text: '其他',
+      icon: 'lightbulb',
+      link: 'other/'
+    }
+  ]
 })
